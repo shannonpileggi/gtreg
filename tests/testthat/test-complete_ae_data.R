d76efaaa2861d60e5214273a761057fe521b0d1d
Shannon Pileggi <pileggis@mskcc.org>
2021-12-21 16:01:03 -0500

    add complete_ae_data tests

--- tests/testthat/test-complete_ae_data.R
+++ tests/testthat/test-complete_ae_data.R
@@ -1,4 +1,6 @@
-test_that("multiplication works", {
+test_that("assess complete data single arm, single soc", {
+
+  # no factor inputs
   df1 <-
     tibble::tibble(
       patient_id = paste0("ID", c(1,1,2,3)),
@@ -8,8 +10,38 @@ test_that("multiplication works", {
       trt = rep("A", 4)
     )
 
+  # introduce missing grade
+  df2 <-
+    tibble::tibble(
+      patient_id = paste0("ID", c(1,1,2,3)),
+      system_organ_class = "Blood and lymphatic system disorders",
+      adverse_event = c("Anaemia", "Anaemia", "Anaemia", "Increased tendency to bruise"),
+      grade = c(1, 1, NA, 2),
+      trt = rep("A", 4)
+    )
 
-  id_df1_correct <-
+   # patient id submitted as factor
+   df3 <-
+    tibble::tibble(
+      patient_id = factor(paste0("ID", c(1,1,2,3))),
+      system_organ_class = "Blood and lymphatic system disorders",
+      adverse_event = c("Anaemia", "Anaemia", "Anaemia", "Increased tendency to bruise"),
+      grade = c(1, 1, 1, 2),
+      trt = rep("A", 4)
+    )
+
+   # strata submitted as factor
+   df4 <-
+     tibble::tibble(
+       patient_id = paste0("ID", c(1,1,2,3)),
+       system_organ_class = "Blood and lymphatic system disorders",
+       adverse_event = c("Anaemia", "Anaemia", "Anaemia", "Increased tendency to bruise"),
+       grade = c(1, 1, 1, 2),
+       trt = factor(rep("A", 4))
+     )
+
+
+  id_valid <-
     tibble::tibble(
       patient_id = paste0("ID", 1:5),
       trt = c(rep("A", 3),rep("B", 2))
@@ -24,15 +56,66 @@ test_that("multiplication works", {
       soc = "system_organ_class",
       by = "grade",
       strata = "trt",
-      id_df = id_df1_correct,
+      id_df = id_valid,
       by_values = as.character(c(1:5))
     )
 
+
+  e2 <-
+    .complete_ae_data(
+      data = df2,
+      id = "patient_id",
+      ae = "adverse_event",
+      soc = "system_organ_class",
+      by = "grade",
+      strata = "trt",
+      id_df = id_valid,
+      by_values = as.character(c(1:5))
+    )
+
+  e2a <-
+    .complete_ae_data(
+      data = df2,
+      id = "patient_id",
+      ae = "adverse_event",
+      soc = "system_organ_class",
+      by = "grade",
+      strata = "trt",
+      id_df = id_valid,
+      by_values = as.character(c(1:5)),
+      missing_text = "who dat"
+    )
+
+  e3 <-
+    .complete_ae_data(
+      data = df3,
+      id = "patient_id",
+      ae = "adverse_event",
+      soc = "system_organ_class",
+      by = "grade",
+      strata = "trt",
+      by_values = as.character(c(1:5))
+    )
+
+
+  e4 <-
+    .complete_ae_data(
+      data = df4,
+      id = "patient_id",
+      ae = "adverse_event",
+      soc = "system_organ_class",
+      by = "grade",
+      strata = "trt",
+      by_values = as.character(c(1:5))
+    )
+
+
   #expect_equal(dim(e1), c(11, 7))
-  # expect_equal(as.character(e1$by), c("1", "1", "2", "dummy", "dummy"))
+  expect_equal(levels(e1$by), c("NOT OBSERVED", "1", "2", "3", "4", "5"))
+  expect_equal(levels(e2$by), c("NOT OBSERVED", "Unknown", "1", "2", "3", "4", "5"))
+  expect_equal(levels(e2a$by), c("NOT OBSERVED", "who dat", "1", "2", "3", "4", "5"))
 
-
-  # identifying incorrect specifications ---------------------------------------
+  # specifications that should result in an error ------------------------------
   # patient id variable name does not match
   expect_error(
     .complete_ae_data(
@@ -50,7 +133,7 @@ test_that("multiplication works", {
     )
   )
 
-  # patient id variable type does not match
+  # patient id variable type does not match: id character, id_df factor
   expect_error(
     .complete_ae_data(
       data = df1,
@@ -63,6 +146,20 @@ test_that("multiplication works", {
         patient_id = factor(paste0("ID", 1:5)),
         trt = c(rep("A", 3),rep("B", 2))
       ),
+      by_values = as.character(c(1:5))
+    )
+  )
+
+  # patient id variable type does not match: id factor, id_df character
+  expect_error(
+    .complete_ae_data(
+      data = df3,
+      id = "patient_id",
+      ae = "adverse_event",
+      soc = "system_organ_class",
+      by = "grade",
+      strata = "trt",
+      id_df = id_valid,
       by_values = as.character(c(1:5))
     )
   )
@@ -84,6 +181,7 @@ test_that("multiplication works", {
     )
   )
 
+  # strata type does not match; input strata is charater, id_df strata is factor
   expect_error(
     .complete_ae_data(
       data = df1,
@@ -101,12 +199,29 @@ test_that("multiplication works", {
   )
 
 
-  # identifying correct specifications -----------------------------------------
+  # specifications that should not result in an error --------------------------
+  expect_error(e1, NA)
+  expect_error(e2, NA)
+  expect_error(e2a, NA)
+  expect_error(e3, NA)
+
+  # similar to e1, but without id_df
   expect_error(
-    e1, NA
+    .complete_ae_data(
+      data = df1,
+      id = "patient_id",
+      ae = "adverse_event",
+      soc = "system_organ_class",
+      by = "grade",
+      strata = "trt"
+    ),
+    NA
   )
 
- # no errors when soc, by, and strata are not specified
+
+ # no errors when soc, by, and strata are not specified ------------------------
+
+  # no soc
   expect_error(
     .complete_ae_data(
       data = df1,
@@ -117,17 +232,8 @@ test_that("multiplication works", {
     ),
     NA
   )
-  expect_error(
-    .complete_ae_data(
-      data = df1,
-      id = "patient_id",
-      ae = "adverse_event",
-      soc = "system_organ_class",
-      by = "grade",
-      strata = "trt"
-    ),
-    NA
-  )
+
+  # no by
   expect_error(
     .complete_ae_data(
       data = df1,
@@ -138,6 +244,8 @@ test_that("multiplication works", {
     ),
     NA
   )
+
+  # no strata
   expect_error(
     .complete_ae_data(
       data = df1,
@@ -148,6 +256,8 @@ test_that("multiplication works", {
     ),
     NA
   )
+
+  # no by, strata, or soc
   expect_error(
     .complete_ae_data(
       data = df1,
