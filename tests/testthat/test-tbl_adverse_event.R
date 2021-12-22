6ae166c376d0865921a933c17ba39a947eca51dc
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-12-22 09:39:31 -0500

    Update test-tbl_adverse_event.R

--- tests/testthat/test-tbl_adverse_event.R
+++ tests/testthat/test-tbl_adverse_event.R
@@ -39,11 +39,54 @@ test_that("df_adverse_events() single arm, single soc", {
   expect_equal(
     e1[["table_body"]][["label"]],
     c("Blood and lymphatic system disorders", "Anaemia", "Increased tendency to bruise")
+  )
+})
+
+test_that("counting rules", {
+  ae_1 <- tibble::tribble(
+    ~subject,     ~cohort, ~soc,            ~ae,             ~grade,
+    "111-03-001", "A",     "Eye disorders", "Eye irritation", 1,
+    "111-03-001", "A",     "Eye disorders", "Eye irritation", 2,
+    "111-03-001", "A",     "Eye disorders", "Vision blurred", 2,
+    "111-03-002", "A",     "Eye disorders", "Vision blurred", 2,
+  )
+
+  ae_2 <- tibble::tribble(
+    ~subject,     ~cohort, ~soc,            ~ae,             ~grade,
+    "111-03-001", "A",     "Eye disorders", "Eye irritation", 1,
+    "111-03-001", "A",     "Eye disorders", "Eye irritation", 2,
+    "111-03-001", "A",     "Eye disorders", "Vision blurred", 2,
+    "111-03-002", "A",     "Eye disorders", "Vision blurred", 2,
+    "111-03-002", "B",     "Gastrointestinal disorders", "Difficult digestion", 1
+  )
+
+
+  # this currently errors out
+  # due to single soc
+  expected_ae_1 <-
+    tbl_adverse_events(
+      data = ae_1,
+      id = subject,
+      ae = ae,
+      soc = soc,
+      by = grade,
+      strata = cohort
+    )
+
+  ## this also errors out
+  expected_ae_2 <-
+    tbl_adverse_events(
+      data = ae_2,
+      id   = subject,
+      ae = ae,
+      soc = soc,
+      by = grade,
+      strata = cohort
     )
 })
 
 # ------------------------------------------------------------------------------
-test_that("df_adverse_events() works", {
+test_that("df_adverse_event() works", {
   expect_error(
     df_adverse_events %>%
       tbl_adverse_event(
