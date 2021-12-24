b60ec6bed437ea8cf1b05264eae38e93d0b6cc60
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-12-24 12:56:28 -0500

    first draft of `tbl_ae_count()`

---
+++ tests/testthat/test-tbl_ae_count.R
@@ -0,0 +1,71 @@
+test_that("tbl_ae_count() works", {
+  expect_error(
+    tbl1 <-
+      tbl_ae_count(
+        data = df_adverse_events,
+        ae = adverse_event,
+        soc = system_organ_class,
+        strata = trt,
+        by = grade,
+        header = "**Grade {level}**"
+      ),
+    NA
+  )
+
+  expect_equal(
+    tbl1$table_body %>%
+      filter(label %in% "Blood and lymphatic system disorders") %>%
+      dplyr::select(gtsummary::all_stat_cols()) %>%
+      c() %>%
+      unname() %>%
+      unlist(),
+    df_adverse_events %>%
+      dplyr::filter(system_organ_class %in% "Blood and lymphatic system disorders") %>%
+      with(table(trt, grade)) %>%
+      t() %>%
+      matrix(nrow = 1) %>%
+      c() %>%
+      as.character()
+  )
+
+  expect_equal(
+    tbl1$table_body %>%
+      filter(label %in% "Anaemia") %>%
+      dplyr::select(gtsummary::all_stat_cols()) %>%
+      c() %>%
+      unname() %>%
+      unlist(),
+    df_adverse_events %>%
+      dplyr::filter(adverse_event %in% "Anaemia") %>%
+      with(table(trt, grade)) %>%
+      t() %>%
+      matrix(nrow = 1) %>%
+      c() %>%
+      as.character()
+  )
+
+  expect_error(
+    tbl_ae_count(
+      data = df_adverse_events,
+      ae = adverse_event,
+      soc = system_organ_class,
+      strata = trt
+    ),
+    NA
+  )
+  expect_error(
+    tbl_ae_count(
+      data = df_adverse_events,
+      ae = adverse_event,
+      soc = system_organ_class
+    ),
+    NA
+  )
+  expect_error(
+    tbl_ae_count(
+      data = df_adverse_events,
+      ae = adverse_event
+    ),
+    NA
+  )
+})
