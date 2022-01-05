5c1b5996b01f013278ab5534708f4b243f6f5e86
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-01-04 19:06:09 -0500

    Added `inline_text()` method for `tbl_ae()` (#60)
    * first draft
    
    * Update inline_text_tbl_ae.R
    
    * updates
    
    * Update DESCRIPTION
    
    * updates
    
    * Update NAMESPACE
    
    * re-document
    
    * Update tests/testthat/test-inline_text.R
    
    Co-authored-by: Shannon Pileggi <skmcclintock@gmail.com>
    
    * updates from review
    
    * updates
    
    Co-authored-by: Shannon Pileggi <skmcclintock@gmail.com>

---
+++ tests/testthat/test-inline_text.R
@@ -0,0 +1,59 @@
+test_that("inline text works", {
+  expect_equal(
+    df_adverse_events %>%
+      tbl_ae(
+        id = patient_id,
+        ae = adverse_event,
+        by = grade,
+        header = "**Grade {level}**"
+      ) %>%
+      inline_text(row = "Anaemia", column = stat_6),
+    "3 (30)"
+  )
+
+  expect_equal(
+    df_adverse_events %>%
+      tbl_ae_count(
+        ae = adverse_event,
+        by = grade,
+        header = "**Grade {level}**"
+      ) %>%
+      inline_text(row = "Anaemia", column = stat_5),
+    "3"
+  )
+
+  expect_equal(
+    df_adverse_events %>%
+      tbl_ae_focus(
+        id = patient_id,
+        ae = adverse_event,
+        include = grade3_complication
+      ) %>%
+      inline_text(row = "Anaemia", column = stat_2_1),
+    "7 (70)"
+  )
+
+  expect_equal(
+    df_adverse_events %>%
+      tbl_ae_focus(
+        id = patient_id,
+        ae = adverse_event,
+        include = grade3_complication
+      ) %>%
+      inline_text(row = "Anaemia"),
+    NULL
+  )
+
+  expect_equal(
+    df_adverse_events %>%
+      tbl_ae(
+        id = patient_id,
+        soc = system_organ_class,
+        ae = adverse_event,
+        by = grade,
+        header = "**Grade {level}**"
+      ) %>%
+      inline_text(row = "Blood and lymphatic system disorders", column = stat_6),
+    "7 (70)"
+  )
+})
