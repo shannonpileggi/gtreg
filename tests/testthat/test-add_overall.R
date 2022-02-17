76b194ed53d35358ebde3290f62153fffd0efa84
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-02-17 12:33:18 -0500

    Update test-add_overall.R

--- tests/testthat/test-add_overall.R
+++ tests/testthat/test-add_overall.R
@@ -92,7 +92,7 @@ test_that("add_overall() warns", {
         statistic = "{n}",
         header_by = "**Grade {level}**"
       ) %>%
-      add_overall(),
+      add_overall(across = "both"),
     "Using `across = 'by'` instead."
   )
 
@@ -133,7 +133,7 @@ test_that("add_overall() warns", {
         strata = trt,
         statistic = "{n}"
       ) %>%
-      add_overall(),
+      add_overall(across = "both"),
     "Using `across = 'strata'` instead."
   )
 
