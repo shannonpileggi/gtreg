65ea3c7cdc4bbe2275dcec5b9c80a843e35a8c4c
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-08-30 09:26:11 -0400

    update default value

--- R/as_table_shell.R
+++ R/as_table_shell.R
@@ -8,7 +8,7 @@
 #' @param pattern regex pattern of text to replace. Default is `"[[:digit:]]+"`
 #' @param replacement string placeholder. Default is `"x"`
 #' @param replace_headers logical indicating whether to replace digits in column headers.
-#' Default is FALSE
+#' Default is FALSE for AE tables and TRUE for all other tables.
 #' @param replace_spanning_headers logical indicating whether to replace digits
 #' in column spanning headers. Default is TRUE
 #'
@@ -33,14 +33,19 @@ as_table_shell <- function(x,
                                                        "ci", "conf.low", "conf.high", "statistic"))),
                            pattern = "[[:digit:]]+",
                            replacement = "xx",
-                           replace_headers = FALSE,
+                           replace_headers = NULL,
                            replace_spanning_headers = TRUE) {
+  # check inputs ---------------------------------------------------------------
   if (!inherits(x, "gtsummary")) {
     cli::cli_abort("Arugment {.code x} must be class {.cls gtsummary}")
   }
 
+  # setting default values -----------------------------------------------------
+  replace_headers <-
+    replace_headers %||%
+    ifelse(inherits(x, c("tbl_ae", "tbl_ae_count", "tbl_ae_focus")), FALSE, TRUE)
 
-  # replace all digits in the table body with 'x'
+  # replace all digits in the table body with 'x' ------------------------------
   to_tibble_and_back_again <-
     x %>%
     gtsummary::modify_column_unhide(everything()) %>%
